4b753f0a0aa254d7b676a216e3824fedf5253630
Aprexx <fizzy6492@gmail.com>
2017-03-23 15:24:18 +0100

    blabla

--- lldpa/lldpAgent.py
+++ lldpa/lldpAgent.py
@@ -75,9 +75,9 @@ class LLDPAgent:
             output += 'LLDPMessage(src_mac='
             output += src[0:2]+":"+src[2:4]+":"+src[4:6]
             tlv = data[14:16]
-            tlv_length = int(0000000111111111,2) & tlv
+            #tlv_length = int(0000000111111111,2) & tlv
             print("tlv_length")
-            print(tlv_length<<7)
+            print(tlv << 7)
 
 
         print(output)
