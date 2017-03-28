ea6649fc5724fdbbd25ea69d7a68c32fbc23d2ed
Aprexx <fizzy6492@gmail.com>
2017-03-28 20:12:39 +0200

    a

--- lldpa/lldpMessage.py
+++ lldpa/lldpMessage.py
@@ -14,9 +14,9 @@ class LLDPMessage(object):
 
     def __str__(self):
         output = 'LLDPMessage(src_mac=' + self.mac + ','
-        output += self.tlv_list(1).__str__() + ','
-        output += self.tlv_list(2).__str__() + ','
-        output += self.tlv_list(3).__str__() + ')'
+        output += self.tlv_list.__getitem__(1).__str__() + ','
+        output += self.tlv_list.__getitem__(2).__str__() + ','
+        output += self.tlv_list.__getitem__(3).__str__() + ')'
         return output
 
     def __repr__(self):
