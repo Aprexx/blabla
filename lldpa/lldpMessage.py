a390a3963b429bdb61814ecb50c6c0b0539e8f7b
Aprexx <fizzy6492@gmail.com>
2017-03-28 13:48:54 +0200

    test maybe fault

--- lldpa/lldpMessage.py
+++ lldpa/lldpMessage.py
@@ -1,4 +1,8 @@
-from lldpa.tlvs import *
+from lldpa.tlvs import base
+from lldpa.tlvs import chassisId
+from lldpa.tlvs import eolldpdu
+from lldpa.tlvs import portId
+from lldpa.tlvs import ttl
 import binascii
 class LLDPMessage(object):
     def __init__(self, src_mac=""):
@@ -35,16 +39,44 @@ class LLDPMessage(object):
         """
         hex_bytes_in = binascii.hexlify(bytes_in)
 
-        while len(hex_bytes_in)>0:
-            tl_string = bin(int(hex_bytes_in[0:4], 16))[2:]
-            type = int(tl_string[0:7].zfill(16), 2)
-            length = int(tl_string[7:16].zfill(16), 2)
-            end = length*2+2
-            #chass = chassisId().load(hex_bytes_in[0:end])
-            break
+        chassis_payload, new_hex, temp_ty = self.extract(1, hex_bytes_in)
+        if temp_ty == 1:
+            self.tlv_list.append(chassisId.TLVChassisId.load(chassis_payload))
+        else:
+            print("wrong tlv order")
+        hex_bytes_in = new_hex
 
-        return bytearray()  # TODO: Implement.
+        port_payload, new_hex, temp_ty = self.extract(1, hex_bytes_in)
+        if temp_ty == 2:
+            self.tlv_list.append(portId.TLVPortId.load(port_payload))
+        else:
+            print("wrong tlv order")
+        hex_bytes_in = new_hex
+
+        ttl_payload, new_hex, temp_ty = self.extract(1, hex_bytes_in)
+        if temp_ty == 3:
+            self.tlv_list.append(ttl.TLVTTL.load(chassis_payload))
+        else:
+            print("wrong tlv order")
+
+        while len(hex_bytes_in) > 0:
+            payload, new_hex, temp_ty = self.extract(hex_bytes_in)
+            if temp_ty == 0:
+                self.tlv_list.append(eolldpdu.TVLEoLLDPDU())
+                break
+            else:
+                self.tlv_list.append(base.LLDPTLV)
+            hex_bytes_in = new_hex
+
+
+        return bytearray()
 
     def dump(self):
         """Dumps all TLVs of the message"""
         return bytearray()  # TODO: Implement.
+
+    def extract(self, data):
+        tl_string = bin(int(data[0:4], 16))[2:].zfill(16)
+        ty = int(tl_string[0:7].zfill(16), 2)
+        le = int(tl_string[7:16].zfill(16), 2)
+        return data[0:le*2+4], data[le*2+4:], ty
