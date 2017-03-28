4ba8b2553afb61cbb74a60a6b893d8ccf66a927e
Aprexx <fizzy6492@gmail.com>
2017-03-28 11:43:07 +0200

    hmmm

--- lldpa/tlvs/chassisId.py
+++ lldpa/tlvs/chassisId.py
@@ -10,7 +10,7 @@ class TLVChassisId(base.LLDPTLV):
             print("chassis subtype != 4 cons")
         self.chassis_id2 = chassis_id
         self.tlv_type = 1
-        self.length = 7
+        self.length2 = 7
 
     def __str__(self):
         """Return a string representation of the TLV"""
@@ -22,7 +22,7 @@ class TLVChassisId(base.LLDPTLV):
         tl_string = bin(int(temp[0:4], 16))[2:].zfill(16)
         print(tl_string)
         self.tlv_type = int(tl_string[0:7].zfill(16), 2)
-        self.length = int(tl_string[7:16].zfill(16), 2)
+        self.length2 = int(tl_string[7:16].zfill(16), 2)
         data = temp[4:]
         self.value = data
         print(self.tlv_type)
@@ -47,7 +47,7 @@ class TLVChassisId(base.LLDPTLV):
         # return binascii.unhexlify(output[2:].zfill(16))
         result = bytearray()
         result.append(struct.pack("!H", self.tlv_type << 1)[1:2])
-        result.append(struct.pack("!H", self.length)[1:2])
+        result.append(struct.pack("!H", self.length2)[1:2])
         result.append(struct.pack("B", self.sub_type2))
         result.extend(binascii.unhexlify(self.chassis_id2.replace(":", "")))
         return result
