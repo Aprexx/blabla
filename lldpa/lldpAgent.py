9274a6bad6dac5bf23bc5171fc4cf2bd7ecd06d2
Aprexx <fizzy6492@gmail.com>
2017-03-31 21:57:23 +0200

    loop

--- lldpa/lldpAgent.py
+++ lldpa/lldpAgent.py
@@ -38,10 +38,11 @@ class LLDPAgent:
         while not self.terminate:
             packet = self.recv_socket.recv(4096)
             self.parse_lldp_frame(packet)
+            i = i+1
             if i == 1:
                 break
             i = i+1
-            break
+
 
         self.recv_socket.close()
 
